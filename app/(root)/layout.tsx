6a454edb6f0ef9fe326e7ad3696eb8e3605c4f2f
heisemmanuell <chidinduemma123@gmail.com>
2024-06-29 20:15:02 -0700

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "letsmeet",
+  description: "Video conferencing app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
